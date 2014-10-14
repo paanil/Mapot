dd7d6364e8a28bdc404c20dad1b8c008919e7149
ilkrpaan <ilari.k.paananen@student.jyu.fi>
2014-10-14 14:35:50 +0300

    DataCollector: Extracting data from requested xml
    Parses requested xml using xml.etree.ElementTree
    Prints interesting data from parsed element tree

--- DataCollector/data_collector.py
+++ DataCollector/data_collector.py
@@ -1,4 +1,5 @@
 import http.client
+import xml.etree.ElementTree as ET
 
 headers = { "Content-Type": "text/xml; charset=utf-8",
             "SOAPAction": "http://ec.europa.eu/eurostat/sri/service/2.0/GetCompactData" }
@@ -21,19 +22,75 @@ def read_query_file(filename):
         query = f.read()
     return query
 
-def build_query_body():
-    query = read_query_file("greenhouse_gas.xml")
+def build_query_body(filename):
+    query = read_query_file(filename)
     return body_begin + query + body_end
 
-def do_query():
+def do_query(filename):
+    body = build_query_body(filename)
+
     connection = http.client.HTTPConnection("data.un.org", 80)
-    body = build_query_body()
     connection.request("POST", "/ws/NSIStdV20Service.asmx", body, headers)
     response = connection.getresponse()
-    print(response.status, response.reason)
     content = response.read().decode("utf-8")
-    print(content.replace(">", ">\n"))
     connection.close()
 
+    print(response.status, response.reason)
+    if response.status is not 200:
+        print("Query " + filename + " failed")
+        return None
+
+    return content
+
+def et_find(elem, tag):
+    for child in elem.getchildren():
+        if tag in child.tag: return child
+        elem = et_find(child, tag)
+        if elem is not None: return elem
+
+    return None
+
+def et_findall(elem, tag):
+    children = []
+    for child in elem.getchildren():
+        if tag in child.tag: children.append(child)
+
+    return children
+
 def main():
-    do_query()
+    ghg_data = do_query("greenhouse_gas.xml")
+    if ghg_data is None: return
+
+    # print(ghg_data.replace(">", ">\n"))
+
+    root = ET.fromstring(ghg_data)
+
+    dataset = et_find(root, "DataSet")
+
+    for series in et_findall(dataset, "Series"):
+        area = series.get("REF_AREA")
+        print(area + ":")
+        for obs in et_findall(series, "Obs"):
+            year = obs.get("TIME_PERIOD")
+            value = obs.get("OBS_VALUE")
+            print("  " + year + ": " + value)
+
+    # ns1 = "{http://schemas.xmlsoap.org/soap/envelope/}"
+    # ns2 = "{http://ec.europa.eu/eurostat/sri/service/2.0}"
+    # ns3 = "{http://www.SDMX.org/resources/SDMXML/schemas/v2_0/message}"
+    # ns4 = "{urn:estat:sdmx.infomodel.keyfamily.KeyFamily=UNSD:DSD_GHG_UNDATA:1.0:compact}"
+    #
+    # root = ET.fromstring(ghg_data)
+    # dataset = root.find(ns1 + "Body").\
+    #     find(ns2 + "GetCompactDataResponse").\
+    #     find(ns2 + "GetCompactDataResult").\
+    #     find(ns3 + "CompactData").\
+    #     find(ns4 + "DataSet")
+    #
+    # for series in dataset.findall(ns4 + "Series"):
+    #     area = series.get("REF_AREA")
+    #     print(area + ":")
+    #     for obs in series.findall(ns4 + "Obs"):
+    #         year = obs.get("TIME_PERIOD")
+    #         value = obs.get("OBS_VALUE")
+    #         print("  " + year + ": " + value)
