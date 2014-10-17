fb5b489e454e5ed74f07929c54bb13df9bbb936f
Markku Kovanen <markku.k.kovanen@student.jyu.fi>
2014-10-17 15:15:15 +0300

    Fixed rendering of countries with multiple polygons

--- 3Dmap/shp2threejs.py
+++ 3Dmap/shp2threejs.py
@@ -2,9 +2,10 @@ import shapefile
 import pyproj
 import svgwrite
 
+
 #shape_path = "/home/makakova/mapdata/naturalEarthCountries/ne_10m_admin_0_countries.shp"
-shape_path = "/home/makakova/mapdata/naturalEarthCountries/ne_50m_admin_0_countries.shp"
-#shape_path = "/home/makakova/mapdata/naturalEarthCountries/ne_110m_admin_0_countries.shp"
+#shape_path = "/home/makakova/mapdata/naturalEarthCountries/ne_50m_admin_0_countries.shp"
+shape_path = "/home/makakova/mapdata/naturalEarthCountries/ne_110m_admin_0_countries.shp"
 #shape_path = "/home/makakova/mapdata/naturalEarthCountriesLakes/ne_110m_admin_0_countries_lakes.shp"
 #shape_path = "/home/makakova/mapdata/land/ne_110m_land.shp"
 selected_attributes = (17,18,19,20)
@@ -15,23 +16,38 @@ sp = pyproj.Proj(init='epsg:4326')
 # Target projection
 tp = pyproj.Proj('+proj=robin +lon_0=0 +lat_0=0 +x_0=0 +y_0=0 +ellps=WGS84 +datum=WGS84 +units=m +no_defs')
 
+def project(point: (int,int)) -> (int,int):
+    """Projects a point from geograpic coordinates to screen coordinates"""
+    x,y = point
+    x,y = pyproj.transform(sp,tp,x,y)
+    return  ((x+17005833)*scale, (y+8625154)*scale)
+
+def separate_polygons(shape) -> [[(int,int)]]:
+    """Separates polygons from a shape-object"""
+    points = shape.points
+    parts = shape.parts
+    parts.append(len(points)) # Includes the last part
+    polygons = []
+    for i,j in zip(parts, parts[1:]):
+        if j == []:
+            j = len(shape.Points)
+        polygons.append(points[i:j])
+    return polygons
 
 sf = shapefile.Reader(shape_path)
 shp_records = sf.shapeRecords()
-# suomi = sf.shape(74)
-# print(suomi.points)
+
 countries = []
 for sr in shp_records:
-    shp = []
-    for p in sr.shape.points:
-        x,y = pyproj.transform(sp,tp,p[0],p[1])
-        coordinates = ((x+17005833)*scale, (y+8625154)*scale)
-        shp.append(coordinates) 
+    polygons = separate_polygons(sr.shape)
+    polygons = [[project(x) for x in xs] for xs in polygons]
     attributes = tuple([sr.record[i] for i in selected_attributes])
-    countries.append((attributes,shp))
+    countries.append((attributes,polygons))
 
 dwg = svgwrite.Drawing(filename="test.svg")
 for c in countries:
     shapes = dwg.add(dwg.g(id=c[0][2]))
-    shapes.add(dwg.polygon(c[1], fill="lime", stroke="black", stroke_width="1"))
+    for polygon in c[1]:
+        shapes.add(dwg.polygon(polygon, fill="lime", stroke="black", stroke_width="1"))
 dwg.save()
+    
