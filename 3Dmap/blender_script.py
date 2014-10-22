1cff769b43dbb0b5c93787b6cb62a43e7997a425
paanil <ilari.k.paananen@student.jyu.fi>
2014-10-23 00:56:37 +0300

    Clean up and fixing
    Fixed blender_script
    Removed svg to json converter because it's not needed anymore
    Updated and rationalized .gitignore

--- 3Dmap/blender_script.py
+++ 3Dmap/blender_script.py
@@ -4,110 +4,120 @@ import json
 import sys
 import os
 
+# Three.js blender export module 'export_threejs.py'
+# note: - it is assumed to be in the same folder with this file
+#       - it must define THREE_exportGeometry custom property like this:
+# bpy.types.Object.THREE_exportGeometry = bpy.props.BoolProperty(default = True)
+#       - add this definition in it or our script won't work
 sys.path.append(os.path.dirname(__file__))
-
 import export_threejs
 
-def export(path):
-    save_path = os.path.dirname(path)
-    sce = bpy.context.scene
-    
-    data = {}
-    for ob in sce.objects:
-        filepath = os.path.join(save_path, ob.name + ".js")
-        
-        text, model_string = export_threejs.generate_mesh_string([ob], sce,
-            True, # option_vertices
-            False, # option_vertices_truncate
-            True, # option_faces
-            False, # option_normals
-            False, # option_uv_coords
-            False, # option_materials
-            False, # option_colors
-            False, # option_bones
-            False, # option_skinning
-            "None", # align_model
-            True, # flipyz
-            1.0, # option_scale
-            True, # export_single_model
-            False, # option_copy_textures
-            filepath, # filepath
-            False, # option_animation_morph
-            False, # option_animation_skeletal
-            False, # option_frame_index_as_time
-            1) # option_frame_step
-    
-        data[ob.name] = json.loads(text)
-     
-    with open(path, "w") as f:
-        f.write(json.dumps(data, separators=(",", ":")))
 
-def build_mesh(me, polygons):
-    extrudeVec = Vector((0.0, 0.0, 1.0))
+def clear_scene(scene):
+    for object in scene.objects:
+        object.select = True
+    bpy.ops.object.delete()
+
+    for mesh in bpy.data.meshes:
+        bpy.data.meshes.remove(mesh)
+
+
+def get_data(shp_path):
+    path = os.path.dirname(__file__)
+    path = os.path.join(path, "shp2threejs.py")
+    with os.popen("python " + path + " " + shp_path) as f:
+        return json.loads(f.read())
+
+
+def build_mesh(mesh, regions):
+    extrude_vec = Vector((0.0, 0.0, 1.0))
     
-    coords = []
+    verts = []
     edges = []
     
-    for points in polygons:
-        first = len(coords)
-        for co in points:
-            index = len(coords)
-            coords.append((co[0], co[1], 0.0))
+    for pointset in regions:
+        first = len(verts)
+        for pt in pointset:
+            index = len(verts)
+            verts.append((pt[0], pt[1], 0.0))
             edges.append([index, index + 1])
         last = len(edges) - 1
         edges[last][1] = first
 
-    me.from_pydata(coords, edges,[])
+    mesh.from_pydata(verts, edges,[])
     
     bpy.ops.object.mode_set(mode = 'EDIT')
-    bpy.ops.mesh.extrude_edges_move(TRANSFORM_OT_translate={"value":extrudeVec})
+    bpy.ops.mesh.extrude_edges_move(TRANSFORM_OT_translate={"value":extrude_vec})
     bpy.ops.mesh.edge_face_add()
     bpy.ops.object.mode_set(mode = 'OBJECT')
 
-def clear_scene():
-    for ob in bpy.context.scene.objects:
-        ob.select = True
-    
-    bpy.ops.object.delete()
-    
-    for me in bpy.data.meshes:
-        bpy.data.meshes.remove(me)
-    
-def run():
-    #filepath = os.path.join(os.path.dirname(__file__), "test.json")
-    
-    #data = shp2threejs.get_shapes_of_countries(19, "C:\\MyTemp\\ne_110m_admin_0_countries\\ne_110m_admin_0_countries.shp")
-    
-    if len(sys.argv) < 2:
-        print("Give .shp file as argument")
-        return
-    
-    
-    shp_path = sys.argv[1]#"C:\\MyTemp\\ne_110m_admin_0_countries\\ne_110m_admin_0_countries.shp"
-    
-    sys.stderr.write(shp_path)
-    
-    clear_scene()
-    
-    #with open("c:\\mytemp\\data.json", "r") as f:
-    #    data = json.loads(f.read())
-    
-    f = os.popen("python shp2threejs.py " + shp_path)
-    data = json.loads(f.read())
-    f.close()
-    
-    for country in data:
-        area = country[0]
-        polygons = country[1]
-        
-        me = bpy.data.meshes.new(area)
-        ob = bpy.data.objects.new(area, me)
-        
-        bpy.context.scene.objects.link(ob)
-        bpy.context.scene.objects.active = ob
-    
-        build_mesh(me, polygons)
-    
-    export("kala.json")
 
-run()
+def create_scene(scene, data):
+    for country in data:
+        name = country[0]
+        regions = country[1]
+
+        mesh = bpy.data.meshes.new(name)
+        object = bpy.data.objects.new(name, mesh)
+
+        scene.objects.link(object)
+        scene.objects.active = object
+
+        build_mesh(mesh, regions)
+
+
+def export_scene(scene, path):
+    data = {}
+
+    for object in scene.objects:
+        file = object.name + ".js"
+
+        text = export_threejs.generate_mesh_string([object], scene,
+            True,   # option_vertices
+            False,  # option_vertices_truncate
+            True,   # option_faces
+            False,  # option_normals
+            False,  # option_uv_coords
+            False,  # option_materials
+            False,  # option_colors
+            False,  # option_bones
+            False,  # option_skinning
+            "None", # align_model
+            True,   # flipyz
+            1.0,    # option_scale
+            True,   # export_single_model
+            False,  # option_copy_textures
+            file,   # filepath
+            False,  # option_animation_morph
+            False,  # option_animation_skeletal
+            False,  # option_frame_index_as_time
+            1)[0]   # option_frame_step
+
+        data[object.name] = json.loads(text)
+
+    with open(path, "w") as f:
+        f.write(json.dumps(data, separators=(",", ":")))
+
+
+def run(shp_file, out_file):
+    data = get_data(shp_file)
+    scene = bpy.context.scene
+    clear_scene(scene)
+    create_scene(scene, data)
+    export_scene(scene, out_file)
+
+
+argv = sys.argv
+argc = len(argv)
+try:
+    argv = argv[argv.index("--"):]
+    argc = len(argv)
+except ValueError:
+    pass
+
+if argc < 2:
+    print("Give .shp file as 1st argument")
+elif argc < 3:
+    print("Give output file as 2nd argument")
+else:
+    run(argv[1], argv[2])
